433348d9723a3a2546f1d4f455854abefcbebb89
Roadhog31 <RoadHog31@users.noreply.github.com>
2018-02-18 20:51:14 +0000

    Add project files.

---
+++ ReadTextFileWhile/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace ReadTextFileWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ReadTextFiles();
+        }
+
+
+        private static void ReadTextFiles()
+        {
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText("Values.txt");
+                string line = string.Empty;
+
+                while (line != null)
+                {
+                    line = sr.ReadToEnd();
+                    if (line != null)
+                        Console.WriteLine(line);
+
+                    sr.Close();
+                    Console.ReadLine();
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();                   
+                }
+            }
+        }
+    }
+}
